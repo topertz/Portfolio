f93c46c1ad65cc5f1a5eb196e3ec141b7cd534c3
topertz <k.markuci@gmail.com>
2026-04-28 11:59:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+const texts = {
+    hu: {
+        title: "Bc. Kovács Márk",
+        about: "Kezdő szoftverfejlesztő vagyok, aki a programozás több területén is szeretne fejlődni. Jelenleg a webfejlesztés mellett foglalkozom Python, JavaScript és C# alapokkal. Célom, hogy átfogó tudást szerezzek különböző programozási nyelvekben és technológiákban. Szeretem a problémamegoldást és motivál, hogy jól strukturált kódot írjak. Szabadidőmben gyakorlok és projekteket készítek.",
+        eduTitle: "Végzettségeim",
+        edu: [
+            "Magyar Tanítási Nyelvű Alapiskola – Deáki (2006.09.01 – 2015.06.30)",
+            "Jedlik Ányos Elektrotechnikai Szakközépiskola – Érsekújvár (2015.09.01 – 2019.06.30)",
+            "Selye János Egyetem – Alkalmazott informatika (Bc.) – Komárom (2021.09.01 – 2024.06.17)",
+            "Selye János Egyetem – Alkalmazott informatika (Mgr.) – Komárom (2024.09.01 – 2026.06.17)"
+        ],
+        projectTitle: "Projektjeim",
+        project: [
+            "Grafilogika alkalmazás",
+            "Készségfejlesztő logikai játék",
+            "Autóiskola projekt",
+            "Repülőgép projekt"
+        ],
+        desc: [
+           "A diplomamunka célja egy olyan alkalmazás létrehozása, amely grafologikus (nonogram, japán keresztrejtvény) típusú rejtvényeket generál és támogatja azok megoldását. Az alkalmazás képes feladatok létrehozására, azok vizuális megjelenítésére, valamint interaktív megoldási lehetőséget, hibakeresést és a helyes megoldás ellenőrzését biztosítja.",
+           "A bakalármunka célja egy egyszerű logikai játék, a Sudoku létrehozása, amely különböző nehézségi szinteken játszható. Probléma esetén adj javaslatot egy lehetséges jó lépésre büntetőpontért cserébe. A játék végén a játék értékeli és rangsorolja a játékost a helyes lépések száma és az eltelt játékidő alapján.",
+           "Ez a projekt az alapvető webfejlesztési koncepciók és az adminisztratív stílusú adatkezelés elsajátítására és gyakorlására készült.",
+           "Ez a projekt algoritmikus problémamegoldás vizsgálatára szolgál közlekedési rendszerekben, különös tekintettel a C++ használatával történő optimalizálásra és ütemezésre."
+        ],
+        contactTitle: "Elérhetőségeim",
+        footer: "Email: kovacs.mark963@gmail.com"
+    },
+
+    sk: {
+        title: "Bc. Márk Kovács",
+        about: "Som začínajúci softvérový vývojár, ktorý sa chce rozvíjať v rôznych oblastiach programovania. Momentálne sa venujem webovému vývoju a učím sa Python, JavaScript a C#. Mojím cieľom je získať široké znalosti v rôznych programovacích jazykoch a technológiách. Rád riešim problémy a motivuje ma písať kvalitný kód. Vo voľnom čase programujem a tvorím projekty.",
+        eduTitle: "Vzdelanie",
+        edu: [
+            "Základná škola s vyučovacím jazykom maďarským – Diakovce (2006.09.01 – 2015.06.30)",
+            "Stredná odborná škola elektrotechnická Jedlika Ányosa – Nové Zámky (2015.09.01 – 2019.06.30)",
+            "Univerzita J. Selyeho – Aplikovaná informatika (Bc.) – Komárno (2021.09.01 – 2024.06.17)",
+            "Univerzita J. Selyeho – Aplikovaná informatika (Mgr.) – Komárno (2024.09.01 – 2026.06.17)"
+        ],
+        projectTitle: "Projekty",
+        project: [
+            "Grafická aplikácia",
+            "Logická hra rozvíjajúca zručnosti",
+            "Projekt autoškoly",
+            "Projekt lietadla"
+        ],
+        desc: [
+            "Cieľom diplomovej práce je vytvoriť aplikáciu, ktorá generuje grafologické (nonogramové, japonské krížovky) hádanky a podporuje ich riešenie. Aplikácia dokáže vytvárať úlohy, ich vizuálny obraz a interaktívne možnosti riešenia, poskytuje detekciu chýb a kontrolu správneho riešenia.",
+            "Cieľom bakalárskej práce je vytvoriť jednoduchú logickú hru Sudoku, ktorú je možné hrať na rôznych úrovniach obtiažnosti. V prípade problému navrhnite možný dobrý ťah výmenou za trestný bod. Na konci hry hra vyhodnotí a zoradí hráča na základe počtu správnych ťahov a uplynutého hracieho času.",
+            "Tento projekt je navrhnutý tak, aby vyučoval a precvičoval základné koncepty webového vývoja a správu dát v administratívnom štýle.",
+            "Tento projekt je zameraný na skúmanie algoritmického riešenia problémov v dopravných systémoch so zameraním na optimalizáciu a plánovanie pomocou jazyka C++."
+        ],
+        contactTitle: "Kontakty",
+        footer: "Email: kovacs.mark963@gmail.com"
+    },
+
+    en: {
+        title: "Bc. Márk Kovács",
+        about: "I am a beginner software developer who wants to grow in multiple areas of programming. I am currently learning Python, JavaScript, and C#. My goal is to gain broad knowledge in different programming languages and technologies. I enjoy problem-solving and writing well-structured code. In my free time, I practice and build projects.",
+        eduTitle: "Education",
+        edu: [
+            "Primary School With Hungarian Language – Diakovce (2006.09.01 – 2015.06.30)",
+            "Jedlik Ányos Technical Secondary School – Nové Zámky (2015.09.01 – 2019.06.30)",
+            "Selye János University – Applied Informatics (Bc.) – Komárno (2021.09.01 – 2024.06.17)",
+            "Selye János University – Applied Informatics (Mgr.) – Komárno (2024.09.01 – 2026.06.17)"
+        ],
+        projectTitle: "Projects",
+        project: [
+            "Graph logic application",
+            "Skill-building logic game",
+            "Driving school project",
+            "Airplane project"
+        ],
+        desc: [
+            "The aim of my diploma thesis is to create an application that generates graphological (nonogram, Japanese crosswords) puzzles and supports their solution. The application is able to create tasks, ich visual images, interactive options, and provides error detection and control management solutions.",
+            "The aim of my bachelor thesis is to create a simple logical Sudoku game that can be played at different levels of difficulty. In case of problem, navrhnite pozji dobý ťah vymánou za trestný bod. Na konci hry hra vyhodnítí a zorádí hátácara na szátő čútő távárních ťahov a uplynutého hracieho času.",
+            "This project is designed to teach precvicoval basic concepts of web development and administration data and administrative style.",
+            "This project is focused on the investigation of algorithmic solutions to problems in transport systems and the optimization of planning using language C++."
+        ],
+        contactTitle: "Contacts",
+        footer: "Email: kovacs.mark963@gmail.com"
+    }
+};
+
+function setLang(lang) {
+    document.getElementById("title").innerText = texts[lang].title;
+    document.getElementById("about").innerText = texts[lang].about;
+    document.getElementById("eduTitle").innerText = texts[lang].eduTitle;
+    document.getElementById("projectTitle").innerText = texts[lang].projectTitle;
+    document.getElementById("contactTitle").innerText = texts[lang].contactTitle;
+    document.getElementById("email").innerText = "kovacs.mark963@gmail.com";
+    const projects = document.getElementsByClassName("project");
+    const descriptions = document.getElementsByClassName("desc");
+    for (let i = 0; i < projects.length; i++) {
+        projects[i].innerText = texts[lang].project[i];
+    }
+    for (let i = 0; i < descriptions.length; i++) {
+        descriptions[i].innerText = texts[lang].desc[i];
+    }
+    const eduSpans = document.querySelectorAll("#edu li span");
+    eduSpans.forEach((el, i) => {
+        el.innerText = texts[lang].edu[i];
+    });
+}
